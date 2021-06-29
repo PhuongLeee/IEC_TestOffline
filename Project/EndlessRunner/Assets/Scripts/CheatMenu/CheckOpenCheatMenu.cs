033ee7b4083705b1562d97e21dd8892fdf97fee3
F:\MySetup\MyApplication\GitHub\SetUp <lepg.hn@gmail.com>
2021-06-30 00:28:09 +0700

    add Cheat Menu

---
+++ Project/EndlessRunner/Assets/Scripts/CheatMenu/CheckOpenCheatMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckOpenCheatMenu : MonoBehaviour
+{
+    private int checkL = 0;
+    [SerializeField] RectTransform cheatPanel;
+    public void CheckOpenCheatLeft() {
+        if (checkL % 2==0) {            
+            checkL++;
+            if (checkL == 5) {
+                cheatPanel.gameObject.SetActive(true);
+            }
+        } else checkL = 0;
+    }
+    public void CheckOpenCheatRight() {
+        if (checkL % 2 != 0) checkL++;
+        else checkL = 0;
+    }
+}
